db5b992b356efe879b1fa2d7c04203e8291820b8
asilbalaban <mail@asilbalaban.com>
2014-03-21 00:40:35 +0200

    Fix indent

--- index.php
+++ index.php
@@ -2,23 +2,23 @@
 
 	set_time_limit(0);
 
-	function domain()
+	function domain($minumumKarakter = 5, $maximumKarakter = 8)
 	{
 		$sesli = ['a','e','i','o','u'];
 		$sessiz = ['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','y','z'];
 
+		$karakterSayisi = rand($minumumKarakter, $maximumKarakter);
+		
 		$domain = '';
-		$karakterSayisi = rand(4,6);
 		for ($i=0;$i<$karakterSayisi;$i++) {
 			if ($i%2==0) {
 				$domain .= $sesli[array_rand($sesli)];
 			} else {
 				$domain .= $sessiz[array_rand($sessiz)];
 			}
-
 		}
 
-		if ( @dns_get_record($domain.'.com', DNS_ALL) ) {
+		if (@dns_get_record($domain.'.com', DNS_ALL)) {
 			$available = 'color: red;';
 		}
 		else {
@@ -26,8 +26,7 @@
 		}
 
 		echo '<h1 style=" '.$available.'  font-family: sans-serif; font-size:72px; text-align:center;">';
-		echo '<a style="text-decoration: none; color: inherit;" href="kaydet.php?domain='.$domain.'">'.$domain.'</a>';
-		echo '.com';
+		echo '<a style="text-decoration: none; color: inherit;" href="save.php?domain='.$domain.'.com">'.$domain.'.com</a>';
 		echo '</h1>';
 	}
 
